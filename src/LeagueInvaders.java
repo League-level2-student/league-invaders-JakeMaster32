802b7bafefb00c7e6b1c58fbe9c920992b28cf20
League Student <league@iMac-29.attlocal.net>
2020-02-10 18:30:04 -0800

    League Invaders IP :smile:

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,20 @@
-
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 public class LeagueInvaders {
 
+	JFrame frame;
+	public static final int Width = 500;
+	public static final int Height = 800;
+	LeagueInvaders() {
+frame = new JFrame();
+	}
+public static void main(String[] args) {
+	LeagueInvaders invader = new LeagueInvaders();
+	invader.setup();
+}
+void setup() {
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(500,800);
+	frame.setVisible(true);
+}
 }
