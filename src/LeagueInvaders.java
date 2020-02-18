222e949882b1d022655c8de1ee57d74e7075e027
League Student <league@iMac-29.attlocal.net>
2020-02-17 18:24:40 -0800

    League Invaders IP :sunglasses:

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,11 +1,12 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class LeagueInvaders {
-
+	GamePanel panel;
 	JFrame frame;
-	public static final int Width = 500;
-	public static final int Height = 800;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
 	LeagueInvaders() {
+	panel = new GamePanel();
 frame = new JFrame();
 	}
 public static void main(String[] args) {
@@ -13,8 +14,10 @@ public static void main(String[] args) {
 	invader.setup();
 }
 void setup() {
+	frame.add(panel);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setSize(500,800);
 	frame.setVisible(true);
+	frame.addKeyListener(panel);
 }
 }
